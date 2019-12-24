b4c37d1ea9b1a933ca9e4abc6f4fa11e8612dc4f
Dobermensch <asharma@connect.ust.hk>
2019-12-24 23:04:35 +0800

    test: bootstrap

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const imgstyle = {
     width: "100%",
@@ -8,31 +10,31 @@ const imgstyle = {
 
 export default () => (
     <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
-      <div className="jumbotron jumbotron-fluid bg-transparent">
-      <img style={imgstyle} src={require("../../assets/images/hk_banner.jpg")} alt="Image of HK" />
-        <div className="container secondary-color">
-          <h1 className="display-4">Minority Experiences: HK</h1>
-          <p className="lead">
-            Experiences of the underrepresented.
-          </p>
-          <hr className="my-4" />
-          <div style={{display: "flex", justifyContent: "space-between"}}>
-            <Link
-              to="/read"
-              className="btn btn-lg custom-button"
-              role="button"
-            >
-              Read Experiences
-            </Link>
-            <Link
-              to="/about"
-              className="btn btn-lg custom-button"
-              role="button"
-            >
-              About Website
-            </Link>
+      <div className="jumbotron jumbotron-fluid" style={{border: "1px solid"}}>
+          <img style={imgstyle} src={require("../../assets/images/hk_banner.jpg")} alt="Image of HK" />
+            <div className="container secondary-color">
+              <h1 className="display-4">Minority Experiences: HK</h1>
+              <p className="lead">
+                Experiences of the underrepresented.
+              </p>
+              <hr className="my-4" />
+              <div style={{display: "flex", justifyContent: "space-between"}}>
+                <Link
+                  to="/read"
+                  className="btn btn-lg custom-button"
+                  role="button"
+                >
+                  Read Experiences
+                </Link>
+                <Link
+                  to="/about"
+                  className="btn btn-lg custom-button"
+                  role="button"
+                >
+                  About Website
+                </Link>
+            </div>
           </div>
-        </div>
       </div>
     </div>
 );
