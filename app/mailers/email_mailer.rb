52bbea1c86df70ae6cc35e18fc6cc9049fa21dc8
Dobermensch <asharma@connect.ust.hk>
2019-12-26 19:26:48 +0800

    fix: email & env var in mailer

--- app/mailers/email_mailer.rb
+++ app/mailers/email_mailer.rb
@@ -1,9 +1,9 @@
 class EmailMailer < ApplicationMailer
-    default from: 'hkinrb@gmail.com'
+    default from: 'hkirnb@gmail.com'
 
     def confirm_email
         @email = params[:email]
-        @url = "#{ENV["SERVER_URL"]}/confirm_exp/#{params[:exp_token]}"
-        mail(to: @email, subject: "HKINRB - Please Confirm That You'd Like To Post The Story")
+        @url = "#{ENV["REACT_APP_API_URL"]}/confirm_exp/#{params[:exp_token]}"
+        mail(to: @email, subject: "ME:HK - Please Confirm That You'd Like To Post The Story")
     end
 end
