faf7dceb8a6e5732c8d089574ff851049806023e
Dobermensch <asharma@connect.ust.hk>
2020-01-17 19:16:21 +0800

    fix: route typo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'homepage#index'
   resources :experiences
   get '/get_ids', to: 'experiences#get_ids'
-  get '/confirm_exp/:id', to: 'experience#confirm_exp' 
+  get '/confirm_exp/:id', to: 'experiences#confirm_exp' 
   get '/read', to: 'homepage#index'
   get '/about', to: 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
