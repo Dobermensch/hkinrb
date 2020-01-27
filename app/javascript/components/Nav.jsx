86f74b29d57cc57e8e5e04885114bad73188a005
Dobermensch <asharma@connect.ust.hk>
2020-01-27 21:11:15 +0800

    mod: responsive nav

--- app/javascript/components/Nav.jsx
+++ app/javascript/components/Nav.jsx
@@ -7,26 +7,30 @@ const navLinkStyle = {
     color: "#aaa",
     display: "block",
     lineHeight: "56px",
-    padding: "0 24px",
+    padding: "0 1vw",
     textDecoration: "none"
 };
+
+const liStyle = {
+    fontSize: "2vw"
+}
 
 class Nav extends Component {
     render() {
         return (
             <nav style={{width: "80%"}}>
-                <h3>ME: HK</h3>
+                <h3 style={{fontSize: "5vh"}}>ME: HK</h3>
                 <ul className="nav-links">
                 <Link style={navLinkStyle} to="/">
-                    <li>Home</li>
+                    <li style={liStyle}>Home</li>
                 </Link>
                 <Link style={navLinkStyle} to="/read">
-                    <li>Read</li>
+                    <li style={liStyle}>Read</li>
                 </Link>
                  <Link style={navLinkStyle} to="/about">
-                    <li>About</li>
+                    <li style={liStyle}>About</li>
                 </Link>
-                {this.props.showPost && <li style={navLinkStyle} style={{cursor: "pointer"}} onClick={this.props.onPostClick}>Post</li>}
+                {this.props.showPost && <li style={navLinkStyle} style={{cursor: "pointer", fontSize: "2vw"}} onClick={this.props.onPostClick}>Post</li>}
                 </ul>
             </nav>
         );
